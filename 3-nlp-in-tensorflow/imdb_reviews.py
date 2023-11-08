5c9557ae12bc0c0b79ef84da71e6b4866d98d503
Reuben Kouidri <reuben-kouidri@hotmail.com>
2023-11-08 07:34:20 +0000

    added lstm model

--- 3-nlp-in-tensorflow/imdb_reviews.py
+++ 3-nlp-in-tensorflow/imdb_reviews.py
@@ -1,16 +1,17 @@
+import io
+import keras.optimizers.legacy as legacy
 import tensorflow_datasets as tfds
 import tensorflow as tf
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-import keras.optimizers.legacy as legacy
-import io
 
 VOCAB_SIZE = 10000
 EMBEDDING_DIM = 16
 MAX_LEN = 128
 TRUNC_TYPE = "post"
 OOV_TOKEN = "<OOV>"
+BATCH_SIZE = 128
 
 
 def train_test_data():
@@ -46,6 +47,17 @@ def get_model():
     return model
 
 
+def lstm_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(VOCAB_SIZE, EMBEDDING_DIM, input_length=MAX_LEN),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+        tf.keras.layers.Dense(6, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(loss='binary_crossentropy', optimizer=legacy.Adam(learning_rate=0.001), metrics=['accuracy'])
+    return model
+
+
 def download_visualisation_data(model, tokenizer):
     embedding_layer = model.layers[0]
     embedding_weights = embedding_layer.get_weights()[0]
@@ -64,16 +76,15 @@ if __name__ == "__main__":
     train_sentences, train_labels, test_sentences, test_labels = train_test_data()
     tokenizer = Tokenizer(num_words=VOCAB_SIZE, oov_token=OOV_TOKEN)
     tokenizer.fit_on_texts(train_sentences)  # tokenizer fit on training set, so test set may contain more OOVs
-    # word_index = tokenizer.word_index
-    # print(word_index)
     sequences = tokenizer.texts_to_sequences(train_sentences)
     padded = pad_sequences(sequences, maxlen=MAX_LEN, truncating=TRUNC_TYPE)
 
     testing_sequences = tokenizer.texts_to_sequences(test_sentences)
     testing_padded = pad_sequences(testing_sequences, maxlen=MAX_LEN, truncating=TRUNC_TYPE)
 
-    model = get_model()
+    # model = get_model()
+    model = lstm_model()
     print(model.summary())
-    model.fit(padded, train_labels, epochs=10, validation_data=(testing_padded, test_labels))
+    model.fit(padded, train_labels, epochs=10, batch_size=BATCH_SIZE, validation_data=(testing_padded, test_labels))
 
     # download_visualisation_data(model, tokenizer)
